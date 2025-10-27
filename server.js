f52db8f71ac76652fd6f65a3455a33eb1f31a084
comercial-pixel <comercial@remyntima.com.br>
2025-10-27 10:14:36 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { sql, getPool } = require('./db');
+// const { sql, getPool } = require('./db');
+const { sql, getPool, dbConfig } = require('./db');
 require('dotenv').config();
 
 const app = express();
